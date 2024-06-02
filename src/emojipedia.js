ca2236bad692001237099dd3dec38c7092dc26ce
Vigi Kun <vighneshkunjir1233@gmail.com>
2024-06-02 14:02:30 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "The grinning face 😀 expresses joy, happiness and satisfaction and is a very common and popular emoji in digital communication. It is often used to simply say hello, to express joy or excitement about something, or to break up a short text. Happiness and good mood are other associations associated with this emoji. In chats, the grinning face can be used to convey positive feelings or a friendly atmosphere. Finally, the 😀 emoji also symbolizes humor and is used to refer to something funny or amusing.",
+  },
+  {
+    id: 5,
+    emoji: "🤐 ",
+    name: "Zipper-Mouth Face",
+    meaning:
+      "Smiley with zipper instead of a mouth is keeping a secret for itself. Lips are closed or chat partner's lips should remain closed. Can also mean silence because you cannot find the right words.",
+  },
+  {
+    id: 6,
+    emoji: "🥱",
+    name: " Yawning face",
+    meaning:
+      "I am dog-tired! The eyes are closed and the hand covers a yawn. The night was too short, you couldn't get enough sleep. Indication that a topic or a person is not of interest, but rather boring.",
+  },
+  {
+    id: 7,
+    emoji: "😴",
+    name: " Sleeping Face",
+    meaning:
+      "Good night and sweet dreams! In comics or cartoons, zZz above the head stands for sleeping characters. The emoji is tired, wants to go to bed or is just about to fall asleep from boredom.",
+  },
+  {
+    id: 8,
+    emoji: "😮‍💨",
+    name: " Smiley exhaling audibly",
+    meaning:
+      "The emoji exhales noisily. Maybe with relief (phew, that was a close thing!) or with disappointment. There is something we may not like at all. Can also mean that the day was very exhausting and now you feel fatigue.",
+  },
+  {
+    id: 9,
+    emoji: "😵 ",
+    name: "Dizzy Face",
+    meaning:
+      "I feel dizzy! Smiley with crossed eyes and open mouth is totally dazed. Is confused and so dizzy it doesn't know which side is up. Also symbolizes strong emotions or drunkenness.",
+  },
+];
+
+export default emojipedia;
